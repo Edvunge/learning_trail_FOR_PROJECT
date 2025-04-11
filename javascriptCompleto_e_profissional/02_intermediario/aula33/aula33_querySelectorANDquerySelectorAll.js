01d636b5faa12ac315f1be94271a436dfd3d66f5
edvaldovunge <costaedy18@gmail.com>
2025-04-11 23:46:34 +0100

    aula33 , aula34

---
+++ javascriptCompleto_e_profissional/02_intermediario/aula33/aula33_querySelectorANDquerySelectorAll.js
@@ -0,0 +1,19 @@
+const divTodas = [...document.getElementsByTagName("div")]
+const cursosC1 = [...document.getElementsByClassName("c1")]
+const cursosC2 = [...document.getElementsByClassName("c2")]
+const cursoEspecial = document.getElementsById("c1")
+
+const query_divTodas = document.querySelector("div")
+
+console.log(divTodas)
+
+/*
+console.log(cursosTodos)
+console.log(divTodas)
+console.log(cursosC1)
+console.log(cursosC2)
+console.log(cursoEspecial)
+*/
+
+
+
